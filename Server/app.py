e58c82d2ac5079fc83ce60e526bb861c757ca77e
Yasser Souri <yassersouri@gmail.com>
2011-12-22 16:09:45 +0330

    add key verification

--- Server/app.py
+++ Server/app.py
@@ -10,16 +10,53 @@ app.config.from_object(__name__)
 
 
 dbg = True
+# dbg = False
 
 @app.route('/')
 def hw():
 	return 'Hey, Do you know how to use this? Or not?'
 
-@app.route('/get/', methods=['GET', 'POST'])
+@app.route('/get/', methods=['POST'])
 def get():
 	arr = []
 	con = db.createCon()
-	con.query("SELECT * FROM transactions WHERE delivered_at IS NULL LIMIT 10")
+	LIMIT = 10 # Default value for a 
+
+	# Validate and Get POST data
+	keys = request.form.keys()
+	print keys
+	has_key = has_name = has_limit = False
+	for key in keys:
+		if key == 'key':
+			has_key = True
+		if key == 'name':
+			has_name = True
+		if key == 'limit':
+			has_limit = True
+	
+	if not has_key or not has_name:
+		return "NO"
+	mobile_key = request.form['key']
+	mobile_name = request.form['name']
+	if has_limit:
+		LIMIT = request.form['limit']
+
+	mobile_query = "SELECT * FROM mobiles WHERE name = \"%s\"" % mobile_name
+	con.query(mobile_query)
+	r = con.store_result()
+	if r.num_rows() < 1:
+		return "NO! your key is not here!"
+	else:
+		row = r.fetch_row()
+		(m_id, m_name, m_key) = row[0]
+		if mobile_key != m_key:
+			return "NO! you have the wrong key!"
+
+
+
+	# RETURN THE MESSAGES
+	get_query = "SELECT * FROM transactions WHERE delivered_at IS NULL LIMIT %d" % LIMIT
+	con.query(get_query)
 	r = con.store_result()
 	for i in xrange(0,r.num_rows()):
 		dic = {}
@@ -44,6 +81,10 @@ def get():
 
 @app.route('/put/', methods=['POST'])
 def put():
+	pass
+
+@app.route('/success/', methods=['POST'])
+def success():
 	pass
 
 if __name__ == '__main__':
