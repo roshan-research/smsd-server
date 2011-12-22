eae4747a208f344b3c7d00ca226cdf1b3668e4cd
Yasser Souri <yassersouri@gmail.com>
2011-12-22 13:20:58 +0330

    update codebase

--- Server/app.py
+++ Server/app.py
@@ -1,6 +1,10 @@
+# coding=utf8
 from flask import Flask
 from flask import request
-import hashlib
+import db
+import json
+import datetime
+
 app = Flask(__name__)
 app.config.from_object(__name__)
 
@@ -13,9 +17,32 @@ def hw():
 
 @app.route('/get/', methods=['GET', 'POST'])
 def get():
-	return str(request.method)
+	arr = []
+	con = db.createCon()
+	con.query("SELECT * FROM transactions WHERE delivered_at IS NULL LIMIT 10")
+	r = con.store_result()
+	for i in xrange(0,r.num_rows()):
+		dic = {}
+		row = r.fetch_row()
+		(m_id, m_to, m_text, c_DT, d_DT, s_DT) = row[0]
 
-@app.route('/put/')
+		# Add to array
+		dic['id'] = m_id
+		dic['to'] = m_to
+		dic['text'] = m_text
+		arr.append(dic)
+
+		# Update Database
+		update_query = "UPDATE transactions SET delivered_at = now() WHERE id = %d" % m_id
+		con.query(update_query)
+		con.commit()
+
+	dic = {}
+	dic['messages'] = arr
+	j = json.dumps(dic)
+	return j
+
+@app.route('/put/', methods=['POST'])
 def put():
 	pass
 
