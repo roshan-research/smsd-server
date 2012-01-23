0fd01bad8c12e7741f69e7a6ba5f1d3485b2a6fe
Alireza Nourian <alireza.nournia@gmail.com>
2012-01-23 13:25:51 +0330

    fix something

--- Mobile/smsd.py
+++ Mobile/smsd.py
@@ -1,7 +1,7 @@
 import json, time, urllib2, urllib
 from threading import Thread
 
-deploy = False # True
+deploy = True
 if deploy:
 	import android
 	dr = android.Android()
@@ -9,6 +9,7 @@ if deploy:
 
 def drSend(to, text):
 	if deploy: dr.smsSend(to, text)
+	print 'sms sent to %s' % to
 
 def drGetMessages():
 	if deploy: return dr.smsGetMessages(True).result
@@ -17,7 +18,7 @@ def drMarkMessageRead(mark):
 	if deploy: dr.smsMarkMessageRead(mark,True)
 
 # config
-pollInterval = 60 if deploy else 1
+pollInterval = 2 if deploy else 1
 deviceInfo = {'name': 'htc-tatto', 'key': '2f1a5ee55fe8435b6aa82782d318f5e2'}
 serverAddress = 'http://192.168.1.4:5000/' # 'http://50.56.222.11:5000/'
 sending = True
@@ -41,37 +42,43 @@ def tSender():
 		time.sleep(pollInterval)
 		if not sending: continue
 
-		# download messages from server
-		res = postData('get/')
-		if not res: continue
-		messages = json.loads(res)['messages']
+		try:
+			# download messages from server
+			res = postData('get/')
+			if not res: continue
+			msgs = json.loads(res)['messages']
 
-		# send messages
-		success = []
-		for message in messages:
-			print message['id'], message['to'], message['text'].encode("utf-8")
-			drSend(message['to'], message['text'].encode("utf-8"))
-			success.append(str(message['id']))
+			# send messages
+			success = []
+			for message in msgs:
+				drSend(message['to'], message['text'].encode("utf-8"))
+				success.append(str(message['id']))
+			msgs = []
 
-		# inform server
-		if len(success) > 0:
-			postData('success/', {'ids': (','.join(success))})
+			# inform server
+			if len(success) > 0:
+				postData('success/', {'ids': (','.join(success))})
+		
+		except Exception as e: print 'send error: ', e
 
 def tReceiver():
 	while 1:
 		time.sleep(1)
 
-		received = drGetMessages()
-		mark = []
-		if received != None:
-			for sms in received:
-				number = sms['address'].strip()
-				text = sms['body'].encode("utf-8")
-				mark.append(int(sms['_id']))
-				cmd = postData('r/', {'from': number, 'text': text})
-				if cmd: applyCommand(cmd)
+		try:
+			received = drGetMessages()
+			mark = []
+			if received != None:
+				for sms in received:
+					number = sms['address'].strip()
+					text = sms['body'].encode("utf-8")
+					mark.append(int(sms['_id']))
+					cmd = postData('r/', {'from': number, 'text': text})
+					if cmd: applyCommand(cmd)
 
-			drMarkMessageRead(mark)
+				drMarkMessageRead(mark)
+
+		except Exception as e: print 'receive error: ', e
 			
 # start threads
 sender = Thread(target=tSender); sender.start()
